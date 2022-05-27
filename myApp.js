62020e0725473f12bfec4c20e083c6361b7a4d69
haqqisadieda <haqqisadiedapandega@gmail.com>
2022-05-27 12:46:52 +0700

    next chapter

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (res, req) => {
-    console.log('Hello World');
+app.get('/', (req, res) => {
+    res.send('Hello Express');
 });
 
 module.exports = app;
