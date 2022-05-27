11294e179809df51e6669ae955dcbd1986325ae8
haqqisadieda <haqqisadiedapandega@gmail.com>
2022-05-27 13:03:54 +0700

    next chapter

--- myApp.js
+++ myApp.js
@@ -2,12 +2,17 @@ let express = require('express');
 let app = express();
 
 const absolutePath = __dirname + '/views/index.html';
+const message = { message: 'Hello json' };
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     console.log(__dirname);
     res.sendFile(absolutePath);
+});
+
+app.get('/json', (req, res) => {
+    res.json(message);
 });
 
 module.exports = app;
