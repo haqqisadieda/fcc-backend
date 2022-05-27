934fcc8ed5318ebd1381aeb054c3c20edbe30bed
haqqisadieda <haqqisadiedapandega@gmail.com>
2022-05-27 13:16:20 +0700

    next chapter

--- myApp.js
+++ myApp.js
@@ -1,18 +1,21 @@
 let express = require('express');
+require('dotenv').config();
 let app = express();
 
 const absolutePath = __dirname + '/views/index.html';
-const message = { message: 'Hello json' };
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    console.log(__dirname);
     res.sendFile(absolutePath);
 });
 
 app.get('/json', (req, res) => {
-    res.json(message);
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ message: 'Hello World'.toUpperCase() });
+    }
+
+    res.json({ message: 'Hello World' });
 });
 
 module.exports = app;
