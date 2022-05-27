059881ae07fe05ec56d661ae4bf437de8b99e6ed
haqqisadieda <haqqisadiedapandega@gmail.com>
2022-05-27 12:52:08 +0700

    next chapter

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-const absolutePath = __dirname + './view/index.html';
+const absolutePath = __dirname + '/view/index.html';
 
 app.get('/', (req, res) => {
     res.sendFile(absolutePath);
